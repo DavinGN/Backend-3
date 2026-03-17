630f80cbe4cab3b20ae1a1c1a884f09aee52c491
DavinGN <220711841@students.uajy.ac.id>
2026-03-17 10:58:24 +0700

    cors?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => RoleMiddleware::class,
         ]);
 
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
