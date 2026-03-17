bfadeb90ee8b735813a5a68236981e2d4e1d6908
DavinGN <220711841@students.uajy.ac.id>
2026-03-17 14:25:25 +0700

    schedule reminder

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('borrow:reminder')->everyFiveMinutes();
+Schedule::command('borrow:reminder')->daily();
 
